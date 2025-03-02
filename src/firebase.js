799bf5c82275a0c057f04127bd0fcba34fd88d6f
malichimo <126613503+malichimo@users.noreply.github.com>
2025-03-02 14:34:15 -0500

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "your-messaging-id",
+  appId: "your-app-id",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
