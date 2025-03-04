f93a93f6c8192994e932fc74c568f5fa724849da
malichimo <126613503+malichimo@users.noreply.github.com>
2025-03-04 14:45:25 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import Login from "./Login.jsx"; // ✅ Import Login Screen
 
 const defaultCenter = { lat: 37.7749, lng: -122.4194 };
-const GOOGLE_MAPS_API_KEY = "YOUR_GOOGLE_MAPS_API_KEY"; // Replace with actual API key
+const GOOGLE_MAPS_API_KEY = "AIzaSyB3m0U9xxwvyl5pax4gKtWEt8PAf8qe9us"; // Replace with actual API key
 
 function App() {
   const [user, setUser] = useState(null);
