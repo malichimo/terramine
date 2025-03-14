1651c9916263d49752558269391b0e1e59da7cb0
malichimo <126613503+malichimo@users.noreply.github.com>
2025-03-13 20:03:28 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,25 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDwh0aVmyCRil5vQTGRMUTER_W37r3r6sM",
-  authDomain: "terramine.onrender.com",
+  authDomain: "terramine-3744d.firebaseapp.com",
   projectId: "terramine-3744d",
   storageBucket: "terramine-3744d.firebasestorage.app",
   messagingSenderId: "746868896165",
   appId: "1:746868896165:web:7c1e0e67ff9054e229784c"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+
+// Initialize Firebase Auth
+const auth = getAuth(app);
+
+// Initialize Google Auth Provider
+const provider = new GoogleAuthProvider();
+
+// Export auth and provider for use in other components
+export { auth, provider };
