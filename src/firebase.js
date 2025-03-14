e558943b22a08ef35b2115e33b1de93470a11e4f
malichimo <126613503+malichimo@users.noreply.github.com>
2025-03-13 20:10:00 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; // Add Firestore import
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,5 +22,8 @@ const auth = getAuth(app);
 // Initialize Google Auth Provider
 const provider = new GoogleAuthProvider();
 
-// Export auth and provider for use in other components
-export { auth, provider };
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export auth, provider, and db for use in other components
+export { auth, provider, db };
