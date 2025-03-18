bd916e5366de6c60a3735b4ea26bfe6f70c312b4
malichimo <scott.w.souza@gmail.com>
2025-03-17 20:09:44 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -329,7 +329,7 @@ function App() {
                     key={terracre.id}
                     position={snappedPosition}
                     icon={{
-                      path: "M -36,-36 L 36,-36 L 36,36 L -36,36 Z", // Adjusted size
+                      path: "M -33,-33 L 33,-33 L 33,33 L -33,33 Z", // Adjusted size
                       scale: getMarkerScale(zoom),
                       fillColor: terracre.ownerId === user.uid ? "blue" : "green",
                       fillOpacity: 1,
