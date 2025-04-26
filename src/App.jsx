46e40004f195cb23bdc9c82d5cea18e9cf2676be
malichimo <scott.w.souza@gmail.com>
2025-04-26 18:51:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -544,7 +544,7 @@ function App() {
                       </Suspense>
 
                       <div className="greeting">
-                        Welcome, {userData?.nickname || "User"}! You have {user?.terrabucks ?? 0} TB.
+                        Welcome, {user?.displayName || "User"}! You have {user?.terrabucks ?? 0} TB.
                       </div>
                       <div className="button-container">
                         <Suspense fallback={<div>Loading buttons...</div>}>
