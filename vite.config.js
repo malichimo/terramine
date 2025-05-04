ca666da09479900baeb08a2a8f63651b59116b5e
malichimo <scott.w.souza@gmail.com>
2025-05-04 18:19:02 -0400

    reduce security

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: '.', // Ensures index.html is read from project root
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-  publicDir: 'public', // Include static assets
-});
+    sourcemap: false, // <- This is the key line
+    chunkSizeWarningLimit: 1000
+  }
+})
+
 
