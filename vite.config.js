f4ee0057d0d0f85fb261c092e8eda97829deb368
malichimo <scott.w.souza@gmail.com>
2025-05-03 21:39:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'esbuild', // Use esbuild to minimize eval usage
+    minify: 'esbuild',
     target: 'esnext',
     sourcemap: true,
   },
   server: {
     hmr: true,
-  },
-  preview: {
-    host: 'terramine.onrender.com', // Allow Render host
-    allowedHosts: ['terramine.onrender.com', 'localhost'], // Explicitly allow these hosts
   },
 });
