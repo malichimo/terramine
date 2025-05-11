313596a8b885c2d40d52619f9a4c222e6180eb42
malichimo <scott.w.souza@gmail.com>
2025-05-11 17:44:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import logo from "/src/assets/terramine logo.png";
+import logo from "./assets/terramine logo.png";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "./firebase";
 
@@ -17,27 +17,14 @@ function App() {
   return (
     <div className="app-container">
       <header className="app-header">
-        <img src={logo} alt="TerraMine Logo" style={{ width: "120px", marginBottom: "20px" }} />
-        <h1>Welcome to TerraMine</h1>
+        <img src={logo} alt="TerraMine Logo" className="logo" />
+        <h1 className="title">Welcome to TerraMine</h1>
+        <button className="google-login-button" onClick={handleGoogleLogin}>
+          Login with Google
+        </button>
       </header>
-      <button
-        style={{
-          fontSize: "16px",
-          padding: "10px 20px",
-          backgroundColor: "#4285F4",
-          color: "white",
-          border: "none",
-          borderRadius: "5px",
-          cursor: "pointer",
-          fontFamily: "Trebuchet MS, sans-serif",
-        }}
-        onClick={handleGoogleLogin}
-      >
-        Login with Google
-      </button>
     </div>
   );
 }
 
 export default App;
-
