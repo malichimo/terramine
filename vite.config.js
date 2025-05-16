7087c0ebc5c348e0b175900fae9f61d6858c299b
malichimo <scott.w.souza@gmail.com>
2025-05-16 16:19:34 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-  },
-  preview: {
-    port: 4173,
-    strictPort: true,
-    allowedHosts: ['terramine.onrender.com'],
-  },
 });
