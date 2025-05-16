65324a893ef8266a43b101cce472660fcf63200c
malichimo <scott.w.souza@gmail.com>
2025-05-16 15:19:35 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,4 +20,3 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 
-console.log("✅ Firebase API Key:", import.meta.env.VITE_API_KEY);
