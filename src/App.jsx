0af2bb1a5a7e08a46b7229d4f9c154b09afb68ff
malichimo <scott.w.souza@gmail.com>
2025-05-31 15:39:54 -0400

    Fix Login import path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { auth, googleProvider, db } from "./firebase";
 
 import MainPage from "./pages/MainPage";
 import UserSetupPage from "./pages/UserSetupPage";
-import Login from "./Login.jsx";
+import Login from "./components/Login.jsx";
 
 export default function App() {
   const [user, setUser] = useState(null);
