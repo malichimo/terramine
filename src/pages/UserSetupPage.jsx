70df1bd9c64a7675a5ab073d6800b76b7f93a843
malichimo <scott.w.souza@gmail.com>
2025-05-31 16:07:51 -0400

    Fix firebase import path in UserSetupPage

--- src/pages/UserSetupPage.jsx
+++ src/pages/UserSetupPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { doc, setDoc } from "firebase/firestore";
-import { db } from "./firebase";
+import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
 export default function UserSetupPage({ user, onSetupComplete }) {
